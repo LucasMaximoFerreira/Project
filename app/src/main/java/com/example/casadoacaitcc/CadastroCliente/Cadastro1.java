28edb894ba86654d1d8999b8095d0a33238aa6e5
Renan <lucas.maximo.ferreira21@gmail.com>
2020-10-29 15:49:08 -0300

    test2

--- app/src/main/java/com/example/casadoacaitcc/CadastroCliente/Cadastro1.java
+++ app/src/main/java/com/example/casadoacaitcc/CadastroCliente/Cadastro1.java
@@ -16,7 +16,7 @@ public class Cadastro1 extends AppCompatActivity implements View.OnClickListener
 
     EditText txtNome, txtSenha, txtEmail, txtData;
     Button btnCad1;
-
+//romeu
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
